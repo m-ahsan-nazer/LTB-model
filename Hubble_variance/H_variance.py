f6ac6e800be1576efbe2530082e4b41a0bd84112
Ahsan- <ihsanahsan@gmail.com>
2014-11-22 00:48:31 +1300

    added code for plotting contours

--- Hubble_variance/H_variance.py
+++ Hubble_variance/H_variance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 import numpy as np
 import healpy as hp
+#https://gist.github.com/davydany/3789221
 
 #The columns in the COMPOSITE sample are as follows:
 #v = cz (km/sec)   d (/h Mpc)  v_pec (km/s)   sigma_v   l (degrees) b (degrees)
@@ -60,6 +61,7 @@ def get_healpix_coords():
 	"""
 	ell, bee = np.loadtxt("pixel_center_galactic_coord_3072.dat",unpack=True)
 	#ell, bee = np.loadtxt("pixel_center_galactic_coord_12288.dat",unpack=True)
+	#ell, bee = np.loadtxt("pixel_center_galactic_coord_49152.dat",unpack=True)
 	
 	return ell*np.pi/180., bee*np.pi/180.
 
@@ -194,16 +196,16 @@ def wrap_smear(cz, sigma,shell_index, ell_hp, bee_hp, inner=True):
 
 radii = np.array([12.5, 15., 20., 30., 40., 50., 60., 70., 80., 90., 100.])
 
-Hs_in     = np.zeros((radii.size,ell_hp.size))
-sigma_in  = np.zeros((radii.size,ell_hp.size))
-Hs_out    = np.zeros((radii.size,ell_hp.size))
-sigma_out = np.zeros((radii.size,ell_hp.size))
+#Hs_in     = np.zeros((radii.size,ell_hp.size))
+#sigma_in  = np.zeros((radii.size,ell_hp.size))
+#Hs_out    = np.zeros((radii.size,ell_hp.size))
+#sigma_out = np.zeros((radii.size,ell_hp.size))
 
 num_cores = mp.cpu_count()-1
 
 def smear_loop(i,radius):
 	shell_index = np.where(dist_comp < radius)[0][-1]
-	cz = cz_comp #cz_lg #cz_comp
+	cz = cz_lg# #cz_comp
 	sigma = sigma_comp
 	
 	Hs_in, sigma_in, Hs_out, sigma_out = \
@@ -235,6 +237,85 @@ for radius, i in zip(radii,xrange(radii.size)):
 	cls_out = cls_out/cls_out[1]
 	sys.stdout.write("%.3f   %.3f    %.3f  %.3f    %.3f  \n" %(radius, cls_in[2], cls_in[3]
 	, cls_out[2], cls_out[3]))
+
+
+
+#hp.mollview(Hs_out[6,:],title="60 in",flip="geo") #,flip="astro",coord='C',
+#hp.mollview(Hs_out[6,:],title="60 out") #,flip="astro",coord='C',
+#plt.show()
+
+
+lon_hp = np.unique(ell_hp)
+lat_hp = np.pi/2 - np.unique(bee_hp)
+nside = hp.npix2nside(bee_hp.size)
+H_map_in  = np.zeros((radii.size,lon_hp.size,lat_hp.size))
+H_map_out = np.zeros((radii.size,lon_hp.size,lat_hp.size))
+#H_map = np.zeros((lon_hp.size,lat_hp.size))
+for i, angle in zip(xrange(lat_hp.size),lat_hp):
+	pixel_indices = hp.ang2pix(nside,angle,lon_hp)
+	for j in xrange(radii.size):
+		H_map_in[j,:,i] = Hs_in[j,pixel_indices]
+		H_map_out[j,:,i] = Hs_out[j,pixel_indices]
+
+
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+from matplotlib import rc
+rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+
+#Basemap works in degrees
+#Go back to galactic coordinates
+lons, lats = np.meshgrid(lon_hp*180./np.pi,90.-lat_hp*180./np.pi)
+
+# create new figure
+#plt.subplots_adjust(left=0.05,right=0.95,top=0.90,bottom=0.05,wspace=0.05,hspace=0.05)
+#fig=plt.figure()
+#fig.suptitle(r"Top to bottom $r_0$: $12.5$, $20$, $40$, $60/h$ Mpc. $($weight free, lg Frame$)$ ")
+# setup of mollweide basemap
+#m = Basemap(resolution='c',projection='moll',lon_0=180.,lat_0=0.,celestial=True)
+##CS1 = m.contour(lons,lats,H_map.transpose(),11,colors='k',latlon=False)#linewidths=0.5
+#num_contours = 11
+#CS2 =\
+#m.contourf(lons,lats,H_map_in[6].transpose(),num_contours,cmap=plt.cm.get_cmap('summer'),extend='both',latlon=True,edgecolors='k')#,CS1.levels
+#gratcolor='#696969'
+#parallels = np.arange(-90.,30,30.)
+#m.drawparallels(parallels,color=gratcolor,linewidth=.5,dashes=[1,1])#,labels=[1,2,3,4])
+#meridians = np.arange(0.,210.,30.)
+#m.drawmeridians(meridians,color=gratcolor,linewidth=.5,dashes=[1,1])
+
+#f, axes = plt.subplots(4,2)
+
+plt.subplots_adjust(left=0.05,right=0.95,top=0.90,bottom=0.05,wspace=0.05,hspace=0.05)
+plt.suptitle(r"Top to bottom $r_0$: $12.5$, $\;20$, $\;40$, $\;60$ $\;h^{-1}$ Mpc. " 
+              r"$($Local group Frame$)$ ",fontsize=16)
+m = Basemap(resolution='c',projection='moll',lon_0=180.,lat_0=0.,celestial=True)
+num_contours = 11
+
+gratcolor='#696969'
+parallels = np.arange(-90.,30,30.)
+m.drawparallels(parallels,color=gratcolor,linewidth=.5,dashes=[1,1])#,labels=[1,2,3,4])
+meridians = np.arange(0.,210.,30.)
+m.drawmeridians(meridians,color=gratcolor,linewidth=.5,dashes=[1,1])
+for i,j in zip([0,2,4,6],[1,3,5,7]):
+	plt.subplot(4,2,j)
+	if ( j == 1 ):
+		plt.title(r"in",fontsize=16)
+	CS2 = m.contourf(lons,lats,H_map_in[i].transpose(),num_contours,
+	cmap=plt.cm.get_cmap('summer'),extend='both',latlon=True,edgecolors='k')
 	
+	plt.subplot(4,2,j+1)
+	if ( j == 1 ):
+		plt.title(r"out",fontsize=16)
+	CS2 = m.contourf(lons,lats,H_map_out[i].transpose(),num_contours,
+	cmap=plt.cm.get_cmap('summer'),extend='both',latlon=True,edgecolors='k')
+
+
+
+plt.show()
+
 
 
